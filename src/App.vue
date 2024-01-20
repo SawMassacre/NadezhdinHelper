36948559732ee468f9b577f971629d3045e22ead
NazarovNikita1 <103824860+SawMassacre@users.noreply.github.com>
2024-01-20 21:30:55 +1000

    feat: First version ready

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@font-face {
+  font-family: "NEXT ART";
+  src: url("./assets/NEXT ART_Regular.otf") format("truetype");
+}
+
+@font-face {
+  font-family: "NEXT ART";
+  src: url("./assets/NEXT ART_Bold.otf") format("opentype");
+  font-weight: bold;
+}
+
+@font-face {
+  font-family: "NEXT ART";
+  src: url("./assets/NEXT ART_SemiBold.otf") format("opentype");
+  font-weight: 600;
+}
+
+body {
+  font-family: "NEXT ART", sans-serif;
+}
+
+strong {
+  font-weight: bold;
+}
+
+.semi-bold {
+  font-weight: 600;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,166 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="main__logo semi-bold" style="margin: 0.5em">
+    <p class="header__title">Надеждин</p>
+    <p class="header__subtitle">2024</p>
+  </div>
+  <div class="page-container">
+    <h1 style="width: 40vw; text-align: center">
+      Это приложение поможет тебе заполнять бланки правильно
+    </h1>
+    <h2 style="width: 45vw; text-align: center">
+      Чтобы проверить как сокращается какое либо слово просто впиши его в поле
+      внизу👇🏻
+    </h2>
+    <div class="input-container">
+      <input
+        type="text"
+        v-model="inputValue"
+        placeholder="Введите текст здесь"
+        class="form__input form__input--centered"
+      />
+    </div>
+    <h1>Правильно будет так:</h1>
+    <div class="output-container">
+      <div
+        class="form__output form__output--centered"
+        style="height: 3em; width: 18em"
+      >
+        {{ abbreviation }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+const dictionary = {
+  город: "г.",
+  "городской округ": "г.о.",
+  "городское поселение": "г.п.",
+  "сельское поселение": "с.п.",
+  район: "р-н",
+  село: "с.",
+  поселок: "п.",
+  "поселок городского типа": "пгт.",
+  деревня: "д.",
+  станица: "ст-ца",
+  хутор: "х.",
+  бульвар: "б-р",
+  квартал: "кв-л",
+  микрорайон: "мкр.",
+  набережная: "наб.",
+  переулок: "пер.",
+  площадь: "пл.",
+  проезд: "пр-д",
+  проспект: "просп.",
+  улица: "ул.",
+  шоссе: "ш.",
+  дом: "д.",
+  корпус: "к.",
+  строение: "стр.",
+  квартира: "кв.",
+  комната: "ком.",
+};
+
+function getAbbreviation(word) {
+  return dictionary[word];
 }
+
+export default {
+  data() {
+    return {
+      inputValue: "",
+    };
+  },
+  computed: {
+    abbreviation() {
+      return getAbbreviation(this.inputValue);
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
+.main__logo {
+  background-image: url("../assets/streak-8a1963b3.webp");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-size: 90%;
+  font-family: NEXT ART, sans-serif;
+  font-weight: 600;
+  font-size: 1.5625em;
+  text-decoration: none;
+  line-height: 0.85;
+  color: currentColor;
+  letter-spacing: -0.05em;
+  padding: 0.75em 0.5em 0.75em 0;
+  margin-right: 1.75em;
   display: inline-block;
-  margin: 0 10px;
+  position: relative;
 }
-a {
-  color: #42b983;
+.header {
+  text-align: left;
+  font-weight: 600;
+  font-size: 1.5625em;
+  text-decoration: none;
+  line-height: 0.85;
+  color: currentColor;
+  letter-spacing: -0.05em;
+  padding: 0.75em 0.5em 0.75em 0;
+  margin-right: 1.75em;
+  display: inline-block;
+  position: relative;
+}
+.form__output {
+  text-align: center;
+}
+
+.form__output--centered {
+  font: inherit;
+  font-size: inherit;
+  font-size: 0.875em;
+  text-transform: uppercase;
+  display: block;
+  width: 100%;
+  border-width: 0;
+  padding: 1em;
+}
+.form__input--centered {
+  text-align: center;
+}
+
+.form__input {
+  font: inherit;
+  font-size: inherit;
+  font-size: 0.875em;
+  text-transform: uppercase;
+  display: block;
+  width: 100%;
+  height: 3em;
+  width: 18em;
+  border-width: 0;
+  padding: 1em;
+}
+
+.output-container {
+  background: #d4e5f0;
+  display: flex;
+  padding: 2em;
+  border-radius: 0.5em;
+}
+
+.input-container {
+  background: #d4e5f0;
+  display: flex;
+  padding: 2em;
+  border-radius: 0.5em;
+}
+.page-container {
+  gap: 2em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 70vh;
+  font-family: "NEXT ART", sans-serif;
 }
 </style>
